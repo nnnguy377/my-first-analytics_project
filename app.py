79c7ff943b8dc94dc6d719ba4be8c6132efa5daa
nnnguy377 <n.nguyen-theuil@tbs-education.org>
2025-03-20 16:54:02 +0000

    visualization car dataset

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
-st.title("Manipulate Streamlit Chart")
+st.title("Import a CSV File and Visualize with Streamlit Charts ")
 
-# Generate random data
-bar_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b","c"])
-st.bar_chart(bar_data)
+df = pd.read_csv("datasets/trips_data_1000.csv")
+st.write(" Preview Uploaded data")
+st.dataframe(df.head())
 
-# Generate random data for line chart
-line_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b","c"])
-st.line_chart(line_data)
+# Chart 1: Bar chart of customers by country
+st.subheader("Customers by City")
+country_counts = df['customer_city'].value_counts()
+st.write(country_counts)
+st.bar_chart(country_counts)
 
-# Generate random data
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b","c"])
-st.scatter_chart(chart_data)
+# Chart 2: Line chart of Tripvcs over time
+st.subheader("Subscriptions Over Time")
+df['Trips Date'] = pd.to_datetime(df['pickup_time']).dt.date
+subscriptions_by_date = df.groupby('Trips Date').size().reset_index(name='Count')
+st.write(subscriptions_by_date)
+st.line_chart(subscriptions_by_date, x="Trips Date", y="Count")
