685d6fad0398ab330eec6daa1adcb4d9f07a87a6
nnnguy377 <n.nguyen-theuil@tbs-education.org>
2025-03-21 09:38:40 +0000

    v2

--- Streamlit/car_sharing_dashboard.py
+++ Streamlit/car_sharing_dashboard.py
@@ -12,6 +12,5 @@ return trips, cars, cities
 trips_merged = trips.merge(cars, on='car_id', how='left')
 trips_merged = trips_merged.merge(cities, on='city_id', how='left')
 
-print(trips_merged.head())
-print(trips_merged.columns)
-print(trips_merged.shape)
+trips_merged = trips_merged.drop(columns=["id_car", "city_id", "id_customer",
+"id"])
