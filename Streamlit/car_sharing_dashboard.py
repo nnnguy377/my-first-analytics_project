536eb8aae49d2b68535512862048d883c16ad194
nnnguy377 <n.nguyen-theuil@tbs-education.org>
2025-03-21 09:36:31 +0000

    v1

--- Streamlit/car_sharing_dashboard.py
+++ Streamlit/car_sharing_dashboard.py
@@ -8,3 +8,10 @@ trips = pd.read_csv("data/trips.csv")
 cars = pd.read_csv("data/cars.csv")
 cities = pd.read_csv("data/cities.csv")
 return trips, cars, cities
+
+trips_merged = trips.merge(cars, on='car_id', how='left')
+trips_merged = trips_merged.merge(cities, on='city_id', how='left')
+
+print(trips_merged.head())
+print(trips_merged.columns)
+print(trips_merged.shape)
